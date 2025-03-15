338031c02e25604765c2dfa008761e93287a3de9
Kristen Johnson <kristenjohnson@MacBook-Air.local>
2025-03-14 20:06:51 -0500

    a lot of work on meeting research assessment stipulations

--- R_scripts/Setup.R
+++ R_scripts/Setup.R
@@ -10,3 +10,13 @@ theme_set(theme_minimal())  # Set default ggplot2 theme
 
 # Print message to confirm script is sourced
 message("setup.R loaded successfully!")
+
+
+
+# load necessary packages
+library(tidyverse) # includes dyplyr, readr, forcats, stringr, ggplot2, tibble, lubridate, tidyr, purrr
+library(flextable) # includes functions for tabular reporting
+library(knitr)
+library(kableExtra)
+library(readxl)
+library(scales)
